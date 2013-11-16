d70fecc9a0d60c692a186b6101817b2eb63ea217
Søren Palmund <palmund@gmail.com>
2013-11-16 09:59:17 +0100

    Swapped license for MIT License.

--- src/net/palmund/logger/LogMessagePrinter.java
+++ src/net/palmund/logger/LogMessagePrinter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 (C) S¿ren Palmund
+ * Copyright 2013 (c) S¿ren Palmund
  * 
  * Licensed under the License described in LICENSE (the "License"); you may not
  * use this file except in compliance with the License.
