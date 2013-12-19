5b630862686eb8b57a115bde0d9225c752832253
Søren Palmund <palmund@gmail.com>
2013-12-19 11:27:40 +0100

    Removed unused imports.

--- test/net/palmund/logger/test/acceptance/LoggerTest_4.java
+++ test/net/palmund/logger/test/acceptance/LoggerTest_4.java
@@ -1,15 +1,10 @@
 package net.palmund.logger.test.acceptance;
 
-import static net.palmund.logger.Logger.JLog;
-import static org.junit.Assert.*;
-
-import net.palmund.logger.LogMessage;
-
 import java.util.Collection;
 
+import net.palmund.logger.LogMessage;
 import net.palmund.logger.Logger;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LoggerTest_4 {
